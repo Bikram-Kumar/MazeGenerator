ecd08718280bb7bd070dfd608192afd3d4845a86
Bikram Kumar <coolbikram0@gmail.com>
2024-09-14 11:39:40 +0530

    dfs working now

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-main();
 
 
 
@@ -6,20 +5,53 @@ function main () {
     var canvas = document.getElementById("cnvs");
     var ctx = canvas.getContext("2d");
     
-    var imgData = new ImageData(256, 256);
+    // var imgData = new ImageData(255, 255);
+
+    var n = 8;
     
-    var canvasDim = new Vector2(256, 256);
+    var canvasDim = new Vector2(n, n);
     var mazeGen = new MazeGenerator(canvasDim);
     var maze = mazeGen.generate();
+    
+    
+    
+    // console.log(maze);
+    var fillStyles = [
+        "#00ff00",
+        "#ffffff",
+    ];
+    
+    var i = 0;
+    drawMaze();
 
-
-    for (var i = 0; i < maze.length; i++) {
-        var index = 4 * ((maze[i].x * 256) + maze[i].y);
-        imgData.data[index] = 255;
-        imgData.data[index+1] = i;
-        imgData.data[index+2] = 105;
-        imgData.data[index+3] = 255;
+    function drawMaze() {
+        ctx.fillStyle = fillStyles[i % fillStyles.length]
+        
+        ctx.fillRect(256/n*maze[i].x, 256/n*maze[i].y, (256/n)/2, (256/n)/2);
+        
+        i++;
+        if (i < maze.length) {
+            setTimeout(() => {
+                drawMaze();
+            }, 100);
+        } else {
+            // location.reload();
+        }
     }
-    console.log (imgData.data);
-    ctx.putImageData(imgData, 0, 0);
+    // console.log (imgData.data);
+    // ctx.putImageData(imgData, 0, 0);
 }
+
+
+
+// adding a swap method to Array prototype for ease
+Array.prototype.swap = function(i, j) {
+    var temp = this[i];
+    this[i] = this[j];
+    this[j] = temp;
+}
+
+
+
+
+main();
--- modules/MazeGenerator.js
+++ modules/MazeGenerator.js
@@ -13,25 +13,38 @@ class MazeGenerator {
             visited[i] = false;
         }
 
-        var queue = [], neigh = [], node = new Vector2(0,0);
-        queue.push(node);
-        while (queue.length) {
+        var neigh = [], node = new Vector2(0,0);
+        var stack = [node];
+        var index;
+        while (stack.length) {
             
-            node = queue.shift();
+            node = stack.pop();
             maze.push(node);
+            index = this.getIndexOf(node);
             
-            if (node.x == 100 && node.y == 220) break;
+            if (visited[index]) {
+
+                continue;
+            }
+            
+            visited[index] = true;
             neigh = this.getNeighbors(node);
-            neigh.forEach((n) => {
-                
-                if ((n != null) && (!visited[(n.x*this.dimension.x) + n.y]) && (!node.equals(n))) {
-                    queue.unshift(n);
-                    visited[(n.x*this.dimension.x) + n.y] = true;
+            this.randomizeNeighborsArray(neigh);
+            console.log("node");
+            console.log(node);
+            // console.log(neigh);
+            
+            for (var n of neigh) {
+                if ((n != null) && (!visited[this.getIndexOf(n)]) && (!node.equals(n))) {
+                    stack.push(n);
+                    console.log(n);
                 }
-            });
+            }
+            
         }
 
         return maze;
+        
         
     }
     
@@ -43,17 +56,35 @@ class MazeGenerator {
         if (vec.y > 0) {
             arr[1] = new Vector2(vec.x, vec.y-1);
         }
-        if (vec.x < this.dimension.x) {
+        if (vec.x < (this.dimension.x - 1)) {
             arr[2] = new Vector2(vec.x+1, vec.y);
         }
-        if (vec.y < this.dimension.y) {
+        if (vec.y < (this.dimension.y - 1)) {
             arr[3] = new Vector2(vec.x, vec.y+1);
         }
         return arr;
     }
 
+    // randomize array by swapping random elements 2 times
+    randomizeNeighborsArray(arr) {
+        var rands = [];
+        for (let i = 0; i < 4; i++) {
+            rands[i] = Math.floor(Math.random() * 4);
+        }
+        arr.swap(rands[0], rands[1]);
+        arr.swap(rands[2], rands[3]);
+    }
+
+    getIndexOf(node) {
+
+        return (node.x + (node.y * this.dimension.x));
+
+    }
+
     get size(){
         return (this.dimension.x * this.dimension.y);
     }
+
+
     
 }
